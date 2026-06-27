ddfba8eb5358c69d20486ac59c8320f48aeb94e3
Madanki <madankimadanki557@gmail.com>
2026-06-27 12:24:13 +0530

    Array 1 code

---
+++ src/Day_7/Arrayvalue/Array1.java
@@ -0,0 +1,12 @@
+package Day_7.Arrayvalue;
+import java.util.Scanner;
+public class Array1 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+    }
+}
