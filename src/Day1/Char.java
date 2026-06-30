45491220790857ab16ec4d31102c9ab4f4c6535a
Madanki <madankimadanki557@gmail.com>
2026-06-30 10:37:53 +0530

    Char code

---
+++ src/Day1/Char.java
@@ -0,0 +1,10 @@
+package Day1;
+
+public class Char {
+        public static void main(String[] args) {
+            char initial = 'c';
+            String name = String.valueOf(initial);
+            System.out.print(name);
+        }
+    }
+
