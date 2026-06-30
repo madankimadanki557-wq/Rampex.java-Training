246582a05b485ff05731fed2fd3633a4729b7058
Madanki <madankimadanki557@gmail.com>
2026-06-30 10:33:20 +0530

    break code

---
+++ src/Day1/Break.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class Break {
+        public static void main(String[] args) {
+            for(int i = 0; i <= 5; i++) {
+                if(i > 2) {
+                    break;
+                }
+                System.out.println(i);
+            }
+        }
+    }
+
