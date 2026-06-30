aacc1d000dc5edd9dea48e18f1958d0be15e5128
Madanki <madankimadanki557@gmail.com>
2026-06-30 11:17:57 +0530

    cgpa

--- src/Day1/Cgpa.java
+++ src/Day1/Cgpa.java
@@ -1,12 +1,11 @@
 package Day1;
 
-public class stu {
+public class Cgpa {
         public static void main(String[] args) {
             int age = 21;
             float cgpa = 7.5f;
             String name = "Madanki";
-            char i='M';
-            System.out.print("My name is "+name+", My initial is "+i+", My age is " + age + ", My cgpa is " + cgpa);
+            System.out.print("My name is " + name + " My age is " + age + " My cgpa is " + cgpa);
         }
     }
 
