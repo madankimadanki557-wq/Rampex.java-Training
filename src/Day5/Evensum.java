65c1a93d5b3704a538b59e85de0aa21cc0fc7b00
Madanki <madankimadanki557@gmail.com>
2026-07-01 12:29:24 +0530

    Even sum code

---
+++ src/Day5/Evensum.java
@@ -0,0 +1,16 @@
+package Day5;
+import java.util.Scanner;
+public class Evensum {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter n: ");
+            int n = sc.nextInt();
+            int sum = 0;
+            for(int i = 2; i <= n; i += 2) {
+                sum += i;
+            }
+            System.out.println("Sum = " + sum);
+            sc.close();
+        }
+    }
+
