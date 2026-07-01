8d366f27368c03cde5b0af8464ee0dabb5c95e9b
Madanki <madankimadanki557@gmail.com>
2026-07-01 12:25:18 +0530

    Traffic code

---
+++ src/Day4/Traffic.java
@@ -0,0 +1,24 @@
+package Day4;
+import java.util.Scanner;
+public class Traffic {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter traffic light color: ");
+            String color = sc.next();
+            switch(color.toLowerCase()) {
+                case "red":
+                    System.out.println("Stop");
+                    break;
+                case "yellow":
+                    System.out.println("Get Ready");
+                    break;
+                case "green":
+                    System.out.println("Go");
+                    break;
+                default:
+                    System.out.println("Invalid Color");
+            }
+            sc.close();
+        }
+    }
+
