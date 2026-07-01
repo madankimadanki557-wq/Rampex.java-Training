21271efd34463b6de22a420db0673f54bab67e23
Madanki <madankimadanki557@gmail.com>
2026-07-01 10:01:54 +0530

    Reverse three digit code

---
+++ src/Day2/Reversethree.java
@@ -0,0 +1,11 @@
+package Day2;
+import java.util.Scanner;
+public class Reversethree {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int rev = (n % 10) * 100 + ((n / 10) % 10) * 10 + (n / 100);
+            System.out.println("Reverse = " + rev);
+        }
+    }
+
