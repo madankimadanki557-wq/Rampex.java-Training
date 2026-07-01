cc0cdb280a967bfa705819bc0f966180cefd094c
Madanki <madankimadanki557@gmail.com>
2026-07-01 15:27:28 +0530

    new code

--- src/Day5/Bulbswitch.java
+++ src/Day5/Bulbswitch.java
@@ -1,9 +1,1 @@
-package Day5;
-import java.util.Scanner;
- public class Bulbswitch {
-     public static void main(String[] args) {
-         static int bulbSwitch ( int n ){
-             return (int) Math.sqrt(n);
-         }
-     }
- }
+
