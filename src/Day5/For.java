5e9c5e5ba8ac8854bd2609f8d2b8050187ab46bb
Madanki <madankimadanki557@gmail.com>
2026-07-01 13:49:15 +0530

    For code

---
+++ src/Day5/For.java
@@ -0,0 +1,10 @@
+package Day5;
+
+public class For {
+        public static void main(String[] args) {
+            for (int i = 1; i <= 5; i++) {
+                System.out.print(6 - i + " ");
+            }
+        }
+    }
+
