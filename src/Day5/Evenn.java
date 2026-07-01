3e42255dc80123219e991652b6a46bf907792382
Madanki <madankimadanki557@gmail.com>
2026-07-01 13:42:54 +0530

    Evenn code

---
+++ src/Day5/Evenn.java
@@ -0,0 +1,12 @@
+package Day5;
+import java.util.Scanner;
+public class Evenn {
+        public static void main(String[] args) {
+            int sum = 0;
+            for (int i = 0; i <= 10; i += 2) {
+                sum += i;
+            }
+            System.out.print(sum);
+        }
+    }
+
