0f0b2e4f94da7845e8c91fa54637439d4c209a81
Madanki <madankimadanki557@gmail.com>
2026-07-01 10:24:38 +0530

    Check char code

--- src/Day3/SimpleInterest.java
+++ src/Day3/SimpleInterest.java
@@ -1,6 +1,6 @@
 package Day3;
 import java.util.Scanner;
-public class SimpleInterest {10
+public class SimpleInterest {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter Principal, Rate and Time: ");
