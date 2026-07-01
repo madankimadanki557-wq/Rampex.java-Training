0348922baeac70e3e1950108f6c860a7eb5589c5
Madanki <madankimadanki557@gmail.com>
2026-07-01 09:55:12 +0530

    class Integer code

---
+++ src/Day2/classInteger.java
@@ -0,0 +1,10 @@
+package Day2;
+
+public class classInteger {
+        public static void main(String[] args) {
+            int a = 10, b = 3;
+            System.out.println("Integer Division = " + (a / b));
+            System.out.println("Floating Division = " + ((double)a / b));
+        }
+    }
+
