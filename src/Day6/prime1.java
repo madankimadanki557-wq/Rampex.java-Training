d4169cdf962a7cd304a76725efe08d284fe7acc2
Madanki <madankimadanki557@gmail.com>
2026-07-06 11:42:06 +0530

    swap number

---
+++ src/Day6/prime1.java
@@ -0,0 +1,26 @@
+package Day6;
+import java.util.Scanner;
+
+public class prime1 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the number:");
+            int n = scanner.nextInt();
+            int count = 0;
+
+            for (int i = 1; i <= n; ++i) {
+                if (n % i == 0) {
+                    ++count;
+                }
+            }
+
+            if (count == 2) {
+                for (int i = 1; i <= n; ++i) {
+                    System.out.println(i);
+                }
+            }
+
+            scanner.close();
+        }
+    }
+
