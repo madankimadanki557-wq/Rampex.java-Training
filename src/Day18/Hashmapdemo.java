377777c3a5b9e0247ad9266ae6a89d542ffbb623
Madanki <madankimadanki557@gmail.com>
2026-07-15 10:15:28 +0530

    bubble sort code

---
+++ src/Day18/Hashmapdemo.java
@@ -0,0 +1,12 @@
+package Day18;
+import java.util.HashMap;
+public class Hashmapdemo {
+        public static void main(String [] args){
+            HashMap<Integer,String> ha=new HashMap<Integer, String>();
+            ha.put(1,"Vishnu");
+            ha.put(2,"Madanki");
+            ha.put(3,"Keerthi");
+            ha.put(4,"Saniya");
+                    System.out.println(ha);
+                }
+            }
