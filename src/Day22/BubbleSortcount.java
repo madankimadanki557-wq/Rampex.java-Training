71d3cf43868b171fa8ce186c583c76e4decb0d20
Madanki <madankimadanki557@gmail.com>
2026-07-15 12:23:49 +0530

    bubble sort new code

--- src/Day22/BubbleSortcount.java
+++ src/Day22/BubbleSortcount.java
@@ -1,6 +1,6 @@
 package Day22;
 import java.util.Arrays;
-class BubbleSort {
+class BubbleSortcount {
     public static void main(String[] args){
         int [] arr= {5,3,1,4,2};
         bubbleSort(arr);
@@ -8,17 +8,25 @@ class BubbleSort {
     public static int [] bubbleSort(int [] arr){
         int n= arr.length;
         int temp=0;
+        int count =0;
+        int swap = 0;
         for(int i=0;i<n-1;i++){
             for(int j=0;j<n-1;j++){
+                count++;
                 if(arr[j]>arr[j+1]){
                     temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swap++;
 
                 }
             }
         }
         System.out.println(Arrays.toString(arr));
+        System.out.println(count);
+
+        System.out.println(swap);
         return arr;
     }
 }
+
